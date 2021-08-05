4f2e35585c12269fe117eb9dc9694eda0ca049bf
Martin Lariz <mlariz@proofpoint.com>
2021-08-05 09:41:24 -0700

    Allow offset for entities, and environment settings for all options.

--- cmd/loadit/main.go
+++ cmd/loadit/main.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"syscall"
 	"time"
+	"strconv"
 
 	"github.com/sensu/sensu-go/agent"
 	"github.com/sensu/sensu-go/types"
@@ -20,18 +21,37 @@ import (
 )
 
 var (
-	flagCount             = flag.Int("count", 1000, "number of concurrent simulated agents")
-	flagBackends          = flag.String("backends", "ws://localhost:8081", "comma separated list of backend URLs")
-	flagNamespace         = flag.String("namespace", agent.DefaultNamespace, "namespace to use for agents")
-	flagSubscriptions     = flag.String("subscriptions", "default", "comma separated list of subscriptions")
-	flagKeepaliveInterval = flag.Int("keepalive-interval", agent.DefaultKeepaliveInterval, "Keepalive interval")
-	flagKeepaliveTimeout  = flag.Int("keepalive-timeout", types.DefaultKeepaliveTimeout, "Keepalive timeout")
-	flagProfilingPort     = flag.Int("pprof-port", 6060, "pprof port to bind to")
-	flagPromBinding       = flag.String("prom", ":8080", "binding for prometheus server")
-	flagUser              = flag.String("user", agent.DefaultUser, "user to authenticate with server")
-	flagPassword          = flag.String("password", agent.DefaultPassword, "password to authenticate with server")
-	flagBaseEntityName    = flag.String("base-entity-name", "test-host", "base entity name to prepend with count number.")
+	flagCount             = flag.Int("count", LookupEnvOrInt("LOADIT_COUNT", 1000), "number of concurrent simulated agents")
+	flagBackends          = flag.String("backends", LookupEnvOrString("LOADIT_BACKENDS", "ws://localhost:8081"), "comma separated list of backend URLs")
+	flagNamespace         = flag.String("namespace", LookupEnvOrString("LOADIT_NAMESPACE", agent.DefaultNamespace), "namespace to use for agents")
+	flagSubscriptions     = flag.String("subscriptions", LookupEnvOrString("LOADIT_SUBSCRIPTIONS", "default"), "comma separated list of subscriptions")
+	flagKeepaliveInterval = flag.Int("keepalive-interval", LookupEnvOrInt("LOADIT_KEEPALIVE_INTERVAL", agent.DefaultKeepaliveInterval), "Keepalive interval")
+	flagKeepaliveTimeout  = flag.Int("keepalive-timeout", LookupEnvOrInt("LOADIT_KEEPALIVE_TIMEOUT", types.DefaultKeepaliveTimeout), "Keepalive timeout")
+	flagProfilingPort     = flag.Int("pprof-port", LookupEnvOrInt("LOADIT_PPROF_PORT", 6060), "pprof port to bind to")
+	flagPromBinding       = flag.String("prom", LookupEnvOrString("LOADIT_PROM", ":8080"), "binding for prometheus server")
+	flagUser              = flag.String("user", LookupEnvOrString("LOADIT_USER", agent.DefaultUser), "user to authenticate with server")
+	flagPassword          = flag.String("password", LookupEnvOrString("LOADIT_PASSWORD", agent.DefaultPassword), "password to authenticate with server")
+	flagBaseEntityName    = flag.String("base-entity-name", LookupEnvOrString("LOADIT_ENTITY_NAME", "test-host"), "base entity name to prepend with count number.")
+  flagEntityOffset      = flag.Int("entity-offset", LookupEnvOrInt("LOADIT_ENTITY_OFFSET", 0), "password to authenticate with server")
 )
+
+func LookupEnvOrString(key string, defaultVal string) string {
+       if val, ok := os.LookupEnv(key); ok {
+               return val
+       }
+       return defaultVal
+}
+
+func LookupEnvOrInt(key string, defaultVal int) int {
+       if val, ok := os.LookupEnv(key); ok {
+               v, err := strconv.Atoi(val)
+               if err != nil {
+                       log.Fatalf("LookupEnvOrInt[%s]: %v", key, err)
+               }
+               return v
+       }
+       return defaultVal
+}
 
 func main() {
 	flag.Parse()
@@ -50,7 +70,7 @@ func main() {
 
 	start := time.Now()
 	for i := 0; i < *flagCount; i++ {
-		name := fmt.Sprintf("%s-%d", *flagBaseEntityName, i+1)
+		name := fmt.Sprintf("%s-%d", *flagBaseEntityName, i+1+*flagEntityOffset)
 
 		cfg := agent.NewConfig()
 		cfg.API.Host = agent.DefaultAPIHost
@@ -93,6 +113,7 @@ func main() {
 
 	elapsed := time.Since(start)
 	fmt.Printf("all agents have been connected in %s\n", elapsed)
+	fmt.Printf("%s-%d ... %s-%d\n", *flagBaseEntityName, 1+*flagEntityOffset, *flagBaseEntityName, *flagCount+*flagEntityOffset)
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
